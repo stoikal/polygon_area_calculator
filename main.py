7e4e82e3473b5da0e31aa51850f2f1adb742c748
Laksito Pamilih <laksito@pm.me>
2021-06-06 11:11:06 +0700

    initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+import shape_calculator
+
+rect = shape_calculator.Rectangle(5, 10)
+print(rect.get_area())
+rect.set_width(3)
+print(rect.get_perimeter())
+print(rect)
+
+sq = shape_calculator.Square(9)
+print(sq.get_area())
+sq.set_side(4)
+print(sq.get_diagonal())
+print(sq)
