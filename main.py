76e526481be472a2e153f02b401da6fd28cfdc75
Laksito Pamilih <laksito@pm.me>
2021-06-06 12:05:14 +0700

    fix amount inside calculation

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
 import shape_calculator
 
-rect = shape_calculator.Rectangle(5, 10)
-print(rect.get_area())
-rect.set_width(3)
-print(rect.get_perimeter())
-print(rect)
+rect2 = shape_calculator.Rectangle(4, 8)
+rect = shape_calculator.Rectangle(3, 6)
 
-sq = shape_calculator.Square(9)
-print(sq.get_area())
-sq.set_side(4)
-print(sq.get_diagonal())
-print(sq)
+print(rect2.get_amount_inside(rect))
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,72 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def __str__(self):
+        name = self.__class__.__name__
+        return f'{name}(width={self.width}, height={self.height})'
+
+    def get_picture(self):
+        width = self.width
+        height = self.height
+        
+        if width > 50 or height > 50:
+            return 'Too big for picture.'
+
+        result_str = '' 
+        for i in range(height):
+            for j in range(width):
+                result_str += '*'
+            # if i < height - 1:
+            result_str += '\n'
+        
+        return result_str
+
+    def set_width(self, width):
+        self.width = width
+    
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_amount_inside(self, rect):
+        amount = 0
+        if rect.width > self.width or rect.height > self.height:
+            return amount
+        
+        amt_x = self.width // rect.width
+        amt_y = self.height // rect.height
+        amount = amt_x * amt_y
 
 
+        return amount
 
+class Square(Rectangle):
+    def __init__(self, side):
+        Rectangle.__init__(self, side, side)
 
-class Square:
+    def __str__(self):
+        name = self.__class__.__name__
+
+        return f'{name}(side={self.width})'
+
+    def set_side(self, side):
+        self.width = side
+        self.height = side
+
+    def set_width(self, width):
+        self.width = width
+        self.height = width
+
+    def set_height(self, height):
+        self.width = height
+        self.height = height
